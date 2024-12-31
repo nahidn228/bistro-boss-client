f7694abe197c4293a948ab8e1a4377e425731097
Nahid Hasan <nahidn228@gmail.com>
2024-12-31 20:22:29 +0600

    Firebase Project Setup and create context

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import AuthProvider from "./providers/AuthProvider";
 import { router } from "./Routes/Routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <div className="max-w-screen-xl mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>
 );
