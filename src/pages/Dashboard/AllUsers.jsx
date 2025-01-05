a1edafef59d5fd1eb8cdadb2e4cd3d489f1eb0b5
Nahid Hasan <nahidn228@gmail.com>
2025-01-05 19:04:35 +0600

    Load all users on the Dashboard page using tanstack query with axios

--- src/pages/Dashboard/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers.jsx
@@ -1,9 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./../../hooks/useAxiosSecure";
+
 const AllUsers = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+
   return (
     <div>
+      {/* header */}
       <div className="flex justify-evenly">
         <h2 className="text-3xl font-semibold"> All Users </h2>
-        <h2 className="text-3xl font-semibold"> Total Users: </h2>
+        <h2 className="text-3xl font-semibold">
+          {" "}
+          Total Users: {users.length}{" "}
+        </h2>
+      </div>
+
+      {/* Table */}
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, idx) => (
+              <tr key={user._id}>
+                <th>{idx + 1}</th>
+                <td>{user.name}</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
