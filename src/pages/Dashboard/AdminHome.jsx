b9fcde7b8182b2dd39f287844ab688b5692b9ddd
Nahid Hasan <nahidn228@gmail.com>
2025-01-12 19:45:30 +0600

    Load data and Display stats on the admin home

--- src/pages/Dashboard/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome.jsx
@@ -1,12 +1,74 @@
+import { useQuery } from "@tanstack/react-query";
+import { FaUsers } from "react-icons/fa";
+import { GiTakeMyMoney } from "react-icons/gi";
 import useAuth from "./../../hooks/useAuth";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import useAxiosSecure from "./../../hooks/useAxiosSecure";
 const AdminHome = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
   return (
     <div>
       <h2 className="text-3xl font-medium">
         <span>Hi, Welcome </span>
         {user?.displayName ? user?.displayName : "Back"}
       </h2>
+      {/* stats */}
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <GiTakeMyMoney className="text-4xl " />
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats?.revenue}</div>
+          <div className="stat-desc">Jan 1st - Now</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-4xl " />
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats?.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <MdProductionQuantityLimits className="text-4xl " />
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats?.menuItem}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-8 w-8 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats?.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
   );
 };
